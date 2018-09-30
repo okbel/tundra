daeae3654f43c333b0b453492243923a746fc675
Belen Curcio <curciobelen@gmail.com>
2018-09-30 20:42:51 -0300

    progress

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,31 @@
 import React, { ReactText, ReactNode } from "react";
+import cn from "classNames";
+import * as styles from "./Button.css";
 
 interface InnerProps {
+  className?: string;
   children: ReactNode | ReactText;
+  size?: "small" | "default" | "large";
+  fullWidth?: boolean;
+  type?: "default" | "filled" | "outlined";
 }
 
 class Button extends React.Component<InnerProps> {
+  public static defaultProps: Partial<InnerProps> = {
+    fullWidth: false,
+    size: "default",
+    type: "default"
+  };
+
   render() {
-    const { children } = this.props;
-    return <button>{children}</button>;
+    const { children, className, fullWidth, type } = this.props;
+
+    const rootClassName = cn(styles.root, className, {
+      [styles.typeDefault]: type,
+      [styles.fullWidth]: fullWidth
+    });
+
+    return <button className={rootClassName}>{children}</button>;
   }
 }
 
